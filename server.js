93bd50c7397689b48acff9e6c9bc4b3d07c4864e
robert <robert.langereis@gmail.com>
2019-10-25 15:46:43 +0200

    deploy attempt

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const connectDB = require('./config/db')
-const path = require('path')
+const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
 
-
-//Connect Database
+// Connect Database
 connectDB();
 
-// init middleware
-app.use(express.json({extended: false}))
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Define Routes
-
-//So, you basically say here: if there is a request for the extensions on api/users, check the file of ./routes/users.js, etc. 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-    //Set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
