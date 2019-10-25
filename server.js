c0925b9b0c3e21ea1bc4167d5004d1b1a26f20c1
robert <robert.langereis@gmail.com>
2019-10-25 14:11:17 +0200

    prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
-
+const path = require('path')
 const app = express();
 
 //Connect Database
@@ -9,16 +9,20 @@ connectDB();
 // init middleware
 app.use(express.json({extended: false}))
 
-app.get('/', (req, res) => res.json({
-    msg: 'Welcome to the Contactkeeper API'
-}));
-
 // Define Routes
 
 //So, you basically say here: if there is a request for the extensions on api/users, check the file of ./routes/users.js, etc. 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
